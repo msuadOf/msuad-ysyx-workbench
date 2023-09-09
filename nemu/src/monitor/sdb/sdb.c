b02f57b640c34b20d118477aa9df7b47040ae88c
tracer-ysyx <tracer@ysyx.org>
2023-09-10 00:11:20 +0800

    > compile NEMU ysyx_22040000 张三 Linux fedora 6.4.14-200.fc38.x86_64 #1 SMP PREEMPT_DYNAMIC Sat Sep  2 16:36:06 UTC 2023 x86_64 GNU/Linux  00:11:20 up 31 min,  3 users,  load average: 0,88, 0,76, 0,50

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -88,8 +88,8 @@ static int cmd_info(char *args) {
 word_t paddr_read(paddr_t addr, int len) ;
 void paddr_write(paddr_t addr, int len, word_t data) ;
 static int cmd_x(char *args) {
-paddr_read(CONFIG_MSIZE, 1) ;
-paddr_write(CONFIG_MSIZE, 1, 1);
+paddr_read(0x80000000, 1) ;
+paddr_write(0x80000000, 1, 1);
   return 0;
 }
 
