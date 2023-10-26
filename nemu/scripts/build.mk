b90018948389f32f6787cac492fad6de44a39809
tracer-ysyx <tracer@ysyx.org>
2023-10-26 23:43:50 +0800

    > compile NEMU ysyx_22040000 白逸凡 Linux Qsuad-UTG 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:43:50 up  1:04,  1 user,  load average: 0.13, 0.27, 0.27

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -28,6 +28,12 @@ LDFLAGS := -O2 $(LDFLAGS)
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
 # Compilation patterns
+$(OBJ_DIR)/%.i: %.c
+	@echo + CC $<
+	@mkdir -p $(dir $@)
+	@$(CC) -fPIE $(CFLAGS) -E -o $@ $<
+	$(call call_fixdep, $(@:.o=.d), $@)
+
 $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
