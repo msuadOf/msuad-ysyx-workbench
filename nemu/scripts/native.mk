938d86d7e5311f003feb229ef0d393ededad5490
tracer-ysyx <tracer@ysyx.org>
2023-11-11 15:52:13 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:52:13 up 3 days, 17:32,  1 user,  load average: 0.01, 0.06, 0.05

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -30,14 +30,13 @@ override ARGS += $(ARGS_DIFF)
 # Command to execute NEMU
 IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
-
+$(echo $(IMG) > args.fl)
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run NEMU")
 	$(RM) args.fl
 	$(echo $(IMG))
-	$(echo $(IMG) > args.fl)
 	$(NEMU_EXEC)
 
 gdb: run-env
