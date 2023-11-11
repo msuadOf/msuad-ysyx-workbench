429419a7ca419a297c0b84f8592a5717a539284b
tracer-ysyx <tracer@ysyx.org>
2023-11-11 18:21:29 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:21:29 up 3 days, 20:01,  1 user,  load average: 0.28, 0.12, 0.08

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -37,3 +37,12 @@ void display_inst() {
   } while ((i = (i+1)%MAX_IRINGBUF) != end);
   puts(ANSI_NONE);
 }
+
+//mtrace
+void trace_mread(paddr_t addr, int len) {
+  printf("pread at " FMT_PADDR " len=%d\n", addr, len);
+}
+
+void trace_mwrite(paddr_t addr, int len, word_t data) {
+  printf("pwrite at " FMT_PADDR " len=%d, data=" FMT_WORD "\n", addr, len, data);
+}
