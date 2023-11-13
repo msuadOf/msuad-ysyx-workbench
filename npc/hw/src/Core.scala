b0780abaf6faa504b8b99a6c318fe7262d28b04b
tracer-ysyx <tracer@ysyx.org>
2023-11-14 01:06:54 +0800

    > generate verilog ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:06:54 up 51 min,  2 users,  load average: 0.20, 0.15, 0.16

--- npc/hw/src/Core.scala
+++ npc/hw/src/Core.scala
@@ -18,6 +18,22 @@ class Core extends Module {
         def apply(idx:Int):UInt={
             regs(idx)
         }
+        val name2idx=Map[String,Int](
+            
+            "$0"->0, "ra"->1, "sp"->2, "gp"->3, "tp"->4, "t0"->5, "t1"->6, "t2"->7,
+            "s0"->8, "s1"->9, "a0"->10, "a1"->11, "a2"->12, "a3"->13, "a4"->14, "a5"->15,
+            "a6"->16, "a7"->17, "s2"->18, "s3"->19, "s4"->20, "s5"->21, "s6"->22, "s7"->23,
+            "s8"->24, "s9"->25, "s10"->26, "s11"->27, "t3"->28, "t4"->29, "t5"->30, "t6"->31
+        )
+        def get_name_by_idx(idx:Int):String={
+            ""
+        }
+        def get_idx_by_name(name:String):Int={
+            name2idx.get(name).get
+        }
+        def apply(name:String):UInt={
+            apply(get_idx_by_name(name))
+        }
     }
     val regFile=new RegFile()
   io.memReadAddress := regFile(1)
