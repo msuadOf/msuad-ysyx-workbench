34c5228526b1a7db040254c0249a824aa9c0f2cd
tracer-ysyx <tracer@ysyx.org>
2023-11-14 00:46:51 +0800

    > generate verilog ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:46:51 up 31 min,  2 users,  load average: 0.42, 0.21, 0.16

--- npc/hw/src/Core.scala
+++ npc/hw/src/Core.scala
@@ -9,14 +9,14 @@ object RegFile{
 
 class Core extends Module {
   val io = IO(new Bundle {
-    val instData        = Input(UInt(32.W))
-    val instAddress        = Output(UInt(32.W)) //pc
+    // val instData        = Input(UInt(32.W))
+    // val instAddress        = Output(UInt(32.W)) //pc
 
-    val memWriteAddress        = Output(UInt(32.W))
-    val memWriteData = Output(UInt(32.W))
+    // val memWriteAddress        = Output(UInt(32.W))
+    // val memWriteData = Output(UInt(32.W))
     
     val memReadAddress        = Output(UInt(32.W))
-    val memReadData     = Input(UInt(32.W))
+    //val memReadData     = Input(UInt(32.W))
   })
 
   io.memReadAddress := RegFile(1)
