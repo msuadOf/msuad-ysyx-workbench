6b8090f9282d5a3bed46fdf0862004f39b6c89b8
tracer-ysyx <tracer@ysyx.org>
2023-11-13 18:01:25 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:01:25 up 5 days, 19:30,  1 user,  load average: 0.02, 0.04, 0.08

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,38 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+char itoa(int num, char* str, int radix)
+{
+    int i = 0;
+    int sum;
+    unsigned int num1 = num;  //如果是负数求补码，必须将他的绝对值放在无符号位中在进行求反码
+    char str1[33] = { 0 };
+    if (num<0) {              //求出负数的补码
+        num = -num;
+        num1 = ~num;
+        num1 += 1;
+    }
+    if (num == 0) {             
+        str1[i] = '0';
+        
+        i++;
+    }
+    while(num1 !=0) {                      //进行进制运算
+        sum = num1 % radix;
+        str1[i] = (sum > 9) ? (sum - 10) + 'a' : sum + '0';
+        num1 = num1 / radix;
+        i++;
+    }
+    i--;
+    int j = 0;
+    for (; i >= 0; i--) {               //逆序输出 
+        str[i] = str1[j];
+        j++;
+    }
+    return strlen(str);
+}
 int sprintf(char *out, const char *fmt, ...) {
-  va_list pArgs;
+    va_list pArgs;
   va_start(pArgs, fmt);
   char *start = out;
   
@@ -25,20 +55,12 @@ int sprintf(char *out, const char *fmt, ...) {
     } else {
       switch (*(++fmt)) {
       case '%': *out = *fmt; ++out; break;
-      case 'd': {
-        int n = va_arg(pArgs, int);
-        char buffer[32];
-        int len = sprintf(buffer, "%d", n);
-        memcpy(out, buffer, len);
-        out += len;
-        break;
-      }
-      case 's': {
+      case 'd': out += itoa(va_arg(pArgs, int), out, 10); break;
+      case 's':
         char *s = va_arg(pArgs, char*);
         strcpy(out, s);
-        out += strlen(s);
+        out += strlen(out);
         break;
-      }
       }
     }
   }
