05777c328b0d77ccb896187055dbc5d2b0e972a5
tracer-ysyx <tracer@ysyx.org>
2023-11-13 17:53:48 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:53:48 up 5 days, 19:23,  1 user,  load average: 0.01, 0.12, 0.12

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,38 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list pArgs;
+  va_start(pArgs, fmt);
+  char *start = out;
+  
+  for (; *fmt != '\0'; ++fmt) {
+    if (*fmt != '%') {
+      *out = *fmt;
+      ++out;
+    } else {
+      switch (*(++fmt)) {
+      case '%': *out = *fmt; ++out; break;
+      case 'd': {
+        int n = va_arg(pArgs, int);
+        char buffer[32];
+        int len = sprintf(buffer, "%d", n);
+        memcpy(out, buffer, len);
+        out += len;
+        break;
+      }
+      case 's': {
+        char *s = va_arg(pArgs, char*);
+        strcpy(out, s);
+        out += strlen(s);
+        break;
+      }
+      }
+    }
+  }
+  *out = '\0';
+  va_end(pArgs);
+
+  return out - start;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
