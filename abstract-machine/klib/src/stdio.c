ddbedf79cf643a79ae383cb6db7b1e7ea0b0c734
tracer-ysyx <tracer@ysyx.org>
2023-11-13 18:09:39 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:09:39 up 5 days, 19:38,  1 user,  load average: 0.06, 0.03, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,61 +13,72 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-char itoa(int num, char* str, int radix)
-{
-    int i = 0;
-    int sum;
-    unsigned int num1 = num;  //如果是负数求补码，必须将他的绝对值放在无符号位中在进行求反码
-    char str1[33] = { 0 };
-    if (num<0) {              //求出负数的补码
-        num = -num;
-        num1 = ~num;
-        num1 += 1;
-    }
-    if (num == 0) {             
-        str1[i] = '0';
-        
-        i++;
-    }
-    while(num1 !=0) {                      //进行进制运算
-        sum = num1 % radix;
-        str1[i] = (sum > 9) ? (sum - 10) + 'a' : sum + '0';
-        num1 = num1 / radix;
-        i++;
-    }
-    i--;
-    int j = 0;
-    for (; i >= 0; i--) {               //逆序输出 
-        str[i] = str1[j];
-        j++;
-    }
-    return strlen(str);
+int intToString(char *out, int num) {
+  int idx = 0;
+
+  if (num < 0) {
+    out[idx++] = '-';
+    num = -num;
+  }
+
+  int divisor = 1;
+  while (num / divisor >= 10) {
+    divisor *= 10;
+  }
+
+  while (divisor > 0) {
+    int digit = num / divisor;
+    out[idx++] = '0' + digit;
+    num %= divisor;
+    divisor /= 10;
+  }
+  
+  out[idx] = '\0';
+  return idx;
+}
+
+int stringCopy(char *out, const char *str) {
+  int idx = 0;
+  while (str[idx] != '\0') {
+    out[idx] = str[idx];
+    idx++;
+  }
+  out[idx] = '\0';
+  return idx;
 }
 int sprintf(char *out, const char *fmt, ...) {
-    va_list pArgs;
-  va_start(pArgs, fmt);
-  char *start = out;
-  
-  for (; *fmt != '\0'; ++fmt) {
-    if (*fmt != '%') {
-      *out = *fmt;
-      ++out;
-    } else {
-      switch (*(++fmt)) {
-      case '%': *out = *fmt; ++out; break;
-      case 'd': out += itoa(va_arg(pArgs, int), out, 10); break;
-      case 's':
-        char *s = va_arg(pArgs, char*);
-        strcpy(out, s);
-        out += strlen(out);
-        break;
-      }
-    }
-  }
-  *out = '\0';
-  va_end(pArgs);
+    va_list args;
+  va_start(args, fmt);
 
-  return out - start;
+  int written = 0;
+  int idx = 0;
+
+  while (fmt[idx] != '\0') {
+    if (fmt[idx] == '%') {
+      idx++;
+      if (fmt[idx] == '\0') {
+        break;
+      } else if (fmt[idx] == '%') {
+        out[written++] = '%';
+      } else if (fmt[idx] == 'd') {
+        int num = va_arg(args, int);
+        written += intToString(out + written, num);
+      } else if (fmt[idx] == 's') {
+        const char *str = va_arg(args, const char *);
+        written += stringCopy(out + written, str);
+      } else {
+        out[written++] = '%';
+        out[written++] = fmt[idx];
+      }
+    } else {
+      out[written++] = fmt[idx];
+    }
+    idx++;
+  }
+
+  out[written] = '\0';
+  va_end(args);
+  return written;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
