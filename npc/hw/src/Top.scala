8caf53cf567f48dafc82a9c90f6097969ac07e88
tracer-ysyx <tracer@ysyx.org>
2023-11-18 15:42:07 +0800

    > generate verilog ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:42:07 up 1 day, 17:52,  1 user,  load average: 0.11, 0.15, 0.08

--- npc/hw/src/Top.scala
+++ npc/hw/src/Top.scala
@@ -11,8 +11,9 @@ class Top extends Module {
   })
 
 
-val reg=Reg(Vec(3,UInt(8.W)))
+val reg=RegInit(VecInit(0.U(8.W),1.U(8.W)))
 reg(0):=io.a
+reg(1):=io.a
 io.b:=reg(0)
   // val regfile=new RegFile(32)
   // io.a:=regfile(2)
