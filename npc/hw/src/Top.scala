5536028e112a566c70738f04fa95d291a23a618d
tracer-ysyx <tracer@ysyx.org>
2023-11-25 01:48:46 +0800

    > generate verilog ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:48:46 up  2:22,  1 user,  load average: 0.02, 0.12, 0.13

--- npc/hw/src/Top.scala
+++ npc/hw/src/Top.scala
@@ -38,7 +38,7 @@ class Top extends Module {
 
   io := DontCare
 
-  val reg = new RegFile("RISCV32E")
+  val R = new RegFile("RISCV32E")
   val pc  = RegInit(0.U(32.W))
 
   //fetch inst
@@ -61,11 +61,16 @@ class Top extends Module {
   // })
 
   //first inst:addi
-  val rs1,rs2,src1R,src2R=Wire(UInt())
+  val rs1,rs2,rd,src1,src2,imm=Wire(UInt())
   rs1 := inst(19, 15)
   rs2 := inst(24, 20)
-  src1R:=reg(rs1)
-  src2R:=reg(rs2)
+  imm:=inst(31,20)
+  rd:=inst(11,7)
+  src1:=R(rs1)
+  src2:=R(rs2)
   println(rs1)
-  println(src1R)
+  println(src1)
+
+  //addi exec
+  R(rd) = src1 + imm
 }
