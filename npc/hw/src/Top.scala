129366a9cbde8c769932d66b3c415f2dcabc72e5
tracer-ysyx <tracer@ysyx.org>
2023-11-25 00:42:42 +0800

    > generate verilog ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:42:42 up  1:16,  1 user,  load average: 0.34, 0.16, 0.06

--- npc/hw/src/Top.scala
+++ npc/hw/src/Top.scala
@@ -1,21 +1,21 @@
 import chisel3._
 import chisel3.util._
 
-class RegFile(val ISet:String){
-  val regNum=ISet match{
+class RegFile(val ISet: String) {
+  val regNum = ISet match {
     case "RISCV32E" => 16
-    case "RISCV32" => 32
-    case "RISCV64" => 64
+    case "RISCV32"  => 32
+    case "RISCV64"  => 64
     case _: String => throw new IllegalArgumentException("RegFile() args should be [RISCV32E] [RISCV32] [RISCV64]")
   }
-  val reg=RegInit(VecInit(Seq.tabulate(regNum)(i => 0.U(32.W))))
-  def apply(idx:Int): UInt ={
+  val reg = RegInit(VecInit(Seq.tabulate(regNum)(i => 0.U(32.W))))
+  def apply(idx: Int): UInt = {
     reg(idx)
   }
-    def apply(idx:UInt): UInt ={
+  def apply(idx: UInt): UInt = {
     reg(idx)
   }
-  import chisel3.experimental.{prefix,SourceInfo}
+  import chisel3.experimental.{prefix, SourceInfo}
   // final def :=(that: => Data)(implicit sourceInfo: SourceInfo): Unit = {
   //     this.:=(that)(sourceInfo)
   // }
@@ -23,29 +23,30 @@ class RegFile(val ISet:String){
 
 class Top extends Module {
   val io = IO(new Bundle {
-    val IMem        = new Bundle {
-      val readAddr=Output(UInt(32.W))
-      val readData=Input(UInt(32.W))
+    val IMem = new Bundle {
+      val readAddr = Output(UInt(32.W))
+      val readData = Input(UInt(32.W))
     }
-      
+
     val DMem = new Bundle {
-      val readAddr=Output(UInt(32.W))
-      val readData=Input(UInt(32.W))
-      val writeAddr=Output(UInt(32.W))
-      val writeData=Output(UInt(32.W))
+      val readAddr  = Output(UInt(32.W))
+      val readData  = Input(UInt(32.W))
+      val writeAddr = Output(UInt(32.W))
+      val writeData = Output(UInt(32.W))
     }
   })
 
-    val reg=new RegFile("RISCV32E")
-    val pc=RegInit(0.U(32.W))
-    io.IMem.readAddr:=pc
-    val inst=io.IMem.readData
+  val reg = new RegFile("RISCV32E")
+  val pc  = RegInit(0.U(32.W))
 
-    RVIInstr.table.map(
-      (m)=>{
-        println(m._1,m._2)
-      }
-    )
-    
+  //fetch inst
+  io.IMem.readAddr := pc
+  val inst = io.IMem.readData
+
+  //decode
+  RVIInstr.table.map((m) => {
+    println(m._1, m._2)
+  })
+  //exec
 
 }
