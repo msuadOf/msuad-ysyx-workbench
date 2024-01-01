fed8ca445801a1ca83ea06531d9579f50bd5d3b8
tracer-ysyx <tracer@ysyx.org>
2024-01-01 17:20:52 +0800

    > sim RTL ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:20:52 up 2 days, 17:20,  1 user,  load average: 0.30, 0.18, 0.12

--- npc/hw/test/verilator/csrc/include/debug.h
+++ npc/hw/test/verilator/csrc/include/debug.h
@@ -24,6 +24,10 @@
     _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
+#define Log_level(level,format, ...) \
+    IFNDEF(concat(CONFIG_Log_level_,level), _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__));
+
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
