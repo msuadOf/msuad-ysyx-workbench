930acd0474b268278fe133f4a68a7189cc835658
tracer-ysyx <tracer@ysyx.org>
2024-01-06 03:40:18 +0800

    > generate verilog ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:40:18 up  2:47,  1 user,  load average: 0.68, 0.45, 0.28

--- npc/hw/src/ebreak.scala
+++ npc/hw/src/ebreak.scala
@@ -5,7 +5,7 @@ class ebreakDpi extends BlackBox with HasBlackBoxResource{
     val io=IO(new Bundle {
         val inst=Input(UInt(32.W))
     })
-    addResource("/chisel_ebreak_dpi.v")
+    addResource("/../../chisel_ebreak_dpi.v")
     // setInline("chisel_ebreak_dpi.v",
     //     """
     //     |import "DPI-C" function void ebreak();
