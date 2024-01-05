a3b0cb4aec1b3ac31ec32846b6660090a795981c
tracer-ysyx <tracer@ysyx.org>
2024-01-06 04:02:06 +0800

    > generate verilog ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:02:06 up  3:08,  1 user,  load average: 0.25, 0.14, 0.18

--- npc/hw/test/verilator/vsrc/chisel_ebreak_dpi.v
+++ npc/hw/test/verilator/vsrc/chisel_ebreak_dpi.v
@@ -3,7 +3,7 @@ module ebreakDpi(
     input wire [31:0] inst
 );
 always @(*) begin
-    if(inst_i == 32'b0000000_00001_00000_000_00000_11100_11) 
+    if(inst == 32'b0000000_00001_00000_000_00000_11100_11) 
     ebreak();       
 end
 endmodule
