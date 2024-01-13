f3d9e8d2411c226176aa1bb5e5b4f8c7f380a300
tracer-ysyx <tracer@ysyx.org>
2024-01-14 03:16:24 +0800

    > sim npc ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:16:24 up 1 day,  2:21,  1 user,  load average: 0.28, 0.25, 0.26

--- npc/hw/test/verilator/csrc/monitor/monitor.cpp
+++ npc/hw/test/verilator/csrc/monitor/monitor.cpp
@@ -147,3 +147,40 @@ int sdb_mainloop(){
   }
     return 0;
 }
+
+static char *log_file = NULL;
+static char *diff_so_file = NULL;
+static char *img_file = NULL;
+
+#include <getopt.h>
+static int parse_args(int argc, char *argv[]) {
+  const struct option table[] = {
+    {"batch"    , no_argument      , NULL, 'b'},
+    {"log"      , required_argument, NULL, 'l'},
+    {"diff"     , required_argument, NULL, 'd'},
+    {"port"     , required_argument, NULL, 'p'},
+    {"help"     , no_argument      , NULL, 'h'},
+    {0          , 0                , NULL,  0 },
+  };
+  int o;
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+    switch (o) {
+    //   case 'b': sdb_set_batch_mode(); break;
+    //   case 'l': log_file = optarg; break;
+      case 'd': diff_so_file = optarg; break;
+    //   case 1: img_file = optarg; return 0;
+      default:
+        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+        printf("\t-b,--batch              run with batch mode\n");
+        printf("\t-l,--log=FILE           output log to FILE\n");
+        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+        printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\n");
+        exit(0);
+    }
+  }
+  return 0;
+}
+int monitor(int argc, char** argv){
+
+}
