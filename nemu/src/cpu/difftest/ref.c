735675a3bc7be9a395bf38ec97b7e598b11d42b7
tracer-ysyx <tracer@ysyx.org>
2024-01-14 05:32:17 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:32:17 up 1 day,  4:37,  1 user,  load average: 0.36, 0.31, 0.30

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -65,11 +65,10 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
   }
   if(direction==DIFFTEST_TO_REF){
     for(int i=0;i<32;i++){
-      
-      //cpu.gpr[i]=s->regs[i];
-      Log("%d:%x",i,s->regs[i]);
+      cpu.gpr[i]=s->regs[i];
+      printf("%d",i);
     }
-    //cpu.pc=s->regs[32];
+    cpu.pc=s->regs[32];
     return;
   }
   assert(0);
