4825a9c7966083d65800e020a5da5c1e28bc8846
tracer-ysyx <tracer@ysyx.org>
2024-01-15 00:38:45 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:38:45 up 1 day, 23:44,  1 user,  load average: 0.20, 0.22, 0.27

--- npc/hw/test/verilator/csrc/difftest/dut.cpp
+++ npc/hw/test/verilator/csrc/difftest/dut.cpp
@@ -27,6 +27,7 @@ void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) =
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
+void (*ref_difftest_reg_display)() = NULL;
 
 
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
@@ -84,6 +85,8 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   ref_difftest_exec = ( void (*)(long unsigned int) ) dlsym(handle, "difftest_exec");
   assert(ref_difftest_exec);
 
+  ref_difftest_reg_display = ( void (*)() ) dlsym(handle, "difftest_reg_display");
+  assert(ref_difftest_exec);
 
   Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
   Log("The result of every instruction will be compared with %s. ", ref_so_file);
