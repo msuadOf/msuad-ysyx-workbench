5c8a64bd85fad1317a41cb8d2864bc25b193b2e4
tracer-ysyx <tracer@ysyx.org>
2024-01-14 18:32:27 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:32:27 up 1 day, 17:37,  1 user,  load average: 1.04, 0.72, 0.39

--- npc/hw/test/verilator/csrc/difftest/dut.cpp
+++ npc/hw/test/verilator/csrc/difftest/dut.cpp
@@ -128,7 +128,7 @@ void difftest_step(CPU_state_diff_t* s) {
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
-  checkregs(&ref_r, s->pc);
+  //checkregs(&ref_r, s->pc);
 }
 #else
 void init_difftest(char *ref_so_file, long img_size, int port) { }
