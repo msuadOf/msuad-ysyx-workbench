c4f0045e94c41df4208ad6e15c0507e94d1585d7
msuad <1445617868@qq.com>
2024-01-24 14:24:51 +0800

    fix bug: disassenmble函数进去就寄，将disasem_init加上就行

--- npc/hw/test/verilator/csrc/difftest/dut.cpp
+++ npc/hw/test/verilator/csrc/difftest/dut.cpp
@@ -23,6 +23,7 @@
 
 #include "mem.h"
 
+void (*ref_difftest_init)(int port) = NULL;
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
@@ -76,6 +77,9 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
 
+  ref_difftest_init = ( void (*)(int) ) dlsym(handle, "difftest_init");
+  assert(ref_difftest_init);
+
   ref_difftest_memcpy = ( void (*)(unsigned int, void*, long unsigned int, bool) ) dlsym(handle, "difftest_memcpy");
   assert(ref_difftest_memcpy);
 
@@ -91,6 +95,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
   Log("The result of every instruction will be compared with %s. ", ref_so_file);
 
+  ref_difftest_init(NULL);
 
   ref_difftest_regcpy(s, DIFFTEST_TO_REF);
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), CONFIG_MSIZE, DIFFTEST_TO_REF);
