2379928f472330b14e9e0b70a4a1b418c77346d2
tracer-ysyx <tracer@ysyx.org>
2024-02-01 20:34:34 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:34:34 up  7:35,  1 user,  load average: 0.67, 0.47, 0.43

--- npc/hw/src/Exec.scala
+++ npc/hw/src/Exec.scala
@@ -1,19 +1,19 @@
 import chisel3._
 import chisel3.util._
 
-class ExecEnv(val inst:UInt,val pc:UInt,val R:RegFile,val DMem:MemIO) {
+class ExecEnv(val inst: UInt, val pc: UInt, val R: RegFile, val DMem: MemIO) {
   //val rs1, rs2, rd, src1, src2, imm ,Rrd = Wire(UInt())
-  val rs1  = inst(19, 15)
-  val rs2  = inst(24, 20)
+  val rs1 = inst(19, 15)
+  val rs2 = inst(24, 20)
 
   val rd   = inst(11, 7)
   val src1 = R(rs1)
-  val src2 = R(rs2) 
+  val src2 = R(rs2)
   val Rrd  = R(rd)
 
-  val immI  = (inst(31, 20).asSInt + 0.S(32.W)).asUInt
-  val immU  = inst(31, 12).asSInt << 12.U
-  val immS  = inst(31, 20)
-  val immJ  = inst(31, 20)
-  val immB  = inst(31, 20)
+  val immI = ( inst(31, 20).asSInt + 0.S(32.W) ).asUInt //imm = SEXT(BITS(i, 31, 20), 12);
+  val immU = inst(31, 12).asSInt << 12.U //imm = SEXT(BITS(i, 31, 12), 20) << 12;
+  val immS = (inst(31, 25).asSInt << 5.U).asUInt | inst(11, 7) //imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7);
+  val immJ =( ((inst(31)<<19 | inst(19,12)<<11  | inst(20)<<10 | inst(30,21) ) <<1).asSInt +0.S(32.W) ).asUInt //imm = SEXT((   (BITS(i, 31, 31) << 19) | BITS(i, 30, 21) | (BITS(i, 20, 20) << 10) | (BITS(i, 19, 12) << 11) ) << 1, 21);
+  val immB = ( (   (inst( 31, 31) << 12) | inst( 30, 25)<<5 | (inst( 11, 8) << 1) | (inst( 7, 7) << 11) ).asSInt + 0.S(32.W) ).asUInt //*imm = SEXT((   (inst( 31, 31) << 12) | BITS(i, 30, 25)<<5 | (BITS(i, 11, 8) << 1) | (BITS(i, 7, 7) << 11) ) , 13);
 }
