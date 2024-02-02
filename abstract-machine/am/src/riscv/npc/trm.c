5e61af27149655cd91def5491e1b889135edaee1
tracer-ysyx <tracer@ysyx.org>
2024-02-02 12:44:47 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:44:47 up  3:03,  1 user,  load average: 0.69, 0.56, 0.34

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,7 +1,7 @@
 #include <am.h>
 #include <klib-macros.h>
 
-# define nemu_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+# define npc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
 extern char _heap_start;
 int main(const char *args);
@@ -20,7 +20,7 @@ void putch(char ch) {
 }
 
 void halt(int code) {
-  nemu_trap(code);
+  npc_trap(code);
   while (1);
 }
 
