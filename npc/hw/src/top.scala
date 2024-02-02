c2b440919017cb2ca11bdbd20074b927280f59df
tracer-ysyx <tracer@ysyx.org>
2024-02-02 14:30:44 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:30:44 up  4:49,  1 user,  load average: 1.51, 0.77, 0.83

--- npc/hw/src/top.scala
+++ npc/hw/src/top.scala
@@ -102,7 +102,7 @@ class top(isa_info: String = "RISCV32") extends Module {
     .asInstanceOf[Array[((BitPat, Any), ExecEnv => Any)]]
     .foreach((t: ((BitPat, Any), ExecEnv => Any)) => {
       prefix(s"InstMatch_${getVariableName(t._1)}") {
-        Decoder.IDLE()
+        //Decoder.IDLE()
         when(t._1._1 === inst) {
           Decoder.IDLE()
           t._2(Decoder)
