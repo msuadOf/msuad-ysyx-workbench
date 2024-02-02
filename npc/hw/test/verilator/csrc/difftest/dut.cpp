f0f9f83151a62996c4843327732019968e933833
tracer-ysyx <tracer@ysyx.org>
2024-02-02 10:05:24 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Qsuad-UTG 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:05:24 up 23 min,  1 user,  load average: 0.79, 0.85, 0.54

--- npc/hw/test/verilator/csrc/difftest/dut.cpp
+++ npc/hw/test/verilator/csrc/difftest/dut.cpp
@@ -96,7 +96,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
   Log("The result of every instruction will be compared with %s. ", ref_so_file);
 
-  ref_difftest_init(NULL);
+  ref_difftest_init(0);
 
   ref_difftest_regcpy(s, DIFFTEST_TO_REF);
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), CONFIG_MSIZE, DIFFTEST_TO_REF);
