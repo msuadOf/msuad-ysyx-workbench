6e053f4e9920dc3595f1708d828b9d9e2542c726
tracer-ysyx <tracer@ysyx.org>
2024-02-12 00:13:56 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Lsuad 4.4.0-22621-Microsoft #2506-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  00:13:56 up 2 days,  2:40,  0 users,  load average: 0.52, 0.58, 0.59

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -27,7 +27,7 @@
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
-      MUXDEF(CONFIG_TARGET_AM, printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__), \
+      MUXDEF(CONFIG_TARGET_AM, Log(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__), \
         (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##  __VA_ARGS__))); \
       IFNDEF(CONFIG_TARGET_AM, extern FILE* log_fp; fflush(log_fp)); \
       extern void assert_fail_msg(); \
