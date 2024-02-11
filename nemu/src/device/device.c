a3b539f9320c0f1800439995af4edf2bf10b320d
tracer-ysyx <tracer@ysyx.org>
2024-02-12 00:12:17 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Lsuad 4.4.0-22621-Microsoft #2506-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  00:12:17 up 2 days,  2:39,  0 users,  load average: 0.52, 0.58, 0.59

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -84,7 +84,7 @@ void init_device() {
   IFDEF(CONFIG_HAS_AUDIO, init_audio());
   IFDEF(CONFIG_HAS_DISK, init_disk());
   IFDEF(CONFIG_HAS_SDCARD, init_sdcard());
-  Log("Here!!!!!!!!");
 
   IFNDEF(CONFIG_TARGET_AM, init_alarm());
+  Log("Here!!!!!!!!");
 }
