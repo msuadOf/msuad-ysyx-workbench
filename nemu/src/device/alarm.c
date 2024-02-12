2d142c7d591ce4b9a07b1b35c178ba56d2f5b30f
tracer-ysyx <tracer@ysyx.org>
2024-02-12 23:05:12 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Lsuad 4.4.0-22621-Microsoft #2506-Microsoft Fri Jan 01 08:00:00 PST 2016 x86_64 x86_64 x86_64 GNU/Linux  23:05:12 up  2:19,  0 users,  load average: 0.52, 0.58, 0.59

--- nemu/src/device/alarm.c
+++ nemu/src/device/alarm.c
@@ -43,10 +43,9 @@ void init_alarm() {
   Assert(ret == 0, "Can not set signal handler");
 
   struct itimerval it = {};
-  struct itimerval it_old = {};
   it.it_value.tv_sec = 0;
   it.it_value.tv_usec = 1000000 / TIMER_HZ;
   it.it_interval = it.it_value;
-  ret = setitimer(ITIMER_VIRTUAL, &it, &it_old);
+  ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
   Assert(ret == 0, "Can not set timer");
 }
