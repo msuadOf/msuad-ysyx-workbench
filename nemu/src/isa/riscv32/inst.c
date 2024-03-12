ec4e5f15c58bac954e50a758121d9c77f4ef7ee4
tracer-ysyx <tracer@ysyx.org>
2024-03-12 18:28:42 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:28:42 up 7 days,  3:23,  1 user,  load average: 0.21, 0.23, 0.15

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -163,7 +163,9 @@ INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , R, MRET );
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
-
+static word_t tmp;
+if(cpu.csr.mstatus!=tmp) Log("changed %08x->%08x",tmp,cpu.csr.mstatus);
+tmp=cpu.csr.mstatus;
   return 0;
 }
 
