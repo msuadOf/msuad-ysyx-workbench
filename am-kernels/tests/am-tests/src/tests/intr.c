fa03b912a259408253f140005c78685822668da6
tracer-ysyx <tracer@ysyx.org>
2024-03-12 07:39:54 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:39:54 up 6 days, 16:34,  1 user,  load average: 0.00, 0.03, 0.09

--- am-kernels/tests/am-tests/src/tests/intr.c
+++ am-kernels/tests/am-tests/src/tests/intr.c
@@ -17,11 +17,10 @@ Context *simple_trap(Event ev, Context *ctx) {
 void hello_intr() {
   printf("Hello, AM World @ " __ISA__ "\n");
   printf("  t = timer, d = device, y = yield\n");
-  //io_read(AM_INPUT_CONFIG);
+  io_read(AM_INPUT_CONFIG);
   iset(1);
   while (1) {
+    for (volatile int i = 0; i < 1000; i++) ;
     yield();
-    for (volatile int i = 0; i < 10000000; i++) ;
-    
   }
 }
