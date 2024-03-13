ea28de499c543ac351a700baad7c6122d4e6c629
tracer-ysyx <tracer@ysyx.org>
2024-03-13 18:14:25 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:14:25 up 8 days,  3:08,  1 user,  load average: 1.31, 1.08, 0.72

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -15,8 +15,9 @@ Context* __am_irq_handle(Context *c) {
     c = user_handler(ev, c);
     assert(c != NULL);
 
-    c->mepc+=1;
-    c->mepc&=~0x1;
+    c->mepc=(uintptr_t)((uint8_t)(c->mepc)+4);
+    // c->mepc+=1;
+    // c->mepc&=~0x1;
 
     if(ev.event == EVENT_ERROR){
       assert(c != NULL);//只写了yield,没有对错误操作的处理，不应该到这里
