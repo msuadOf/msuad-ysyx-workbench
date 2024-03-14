e77106736284117dd147dd68bc88aa016216bf4b
tracer-ysyx <tracer@ysyx.org>
2024-03-14 16:11:15 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:11:15 up 9 days,  1:05,  1 user,  load average: 0.76, 0.43, 0.27

--- rt-thread-am/bsp/abstract-machine/src/context.c
+++ rt-thread-am/bsp/abstract-machine/src/context.c
@@ -94,9 +94,9 @@ __attribute__((unused)) static void wrap_entry(void *params)
   wrap_func_params_t *p = (wrap_func_params_t *)params;
 
   p->tentry(p->parameter); // 调用入口函数
+asm("wrap_entry_texit:");
+  p->texit();              // 在tentry返回后调用退出函数
 
- wrap_entry_texit:  p->texit();              // 在tentry返回后调用退出函数
-goto wrap_entry_texit;
   while (1)
   {
   }
