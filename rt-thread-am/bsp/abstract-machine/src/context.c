84f87b6a044d3301a5681251618c684557689392
tracer-ysyx <tracer@ysyx.org>
2024-03-14 13:22:10 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:22:10 up 8 days, 22:16,  1 user,  load average: 0.57, 0.30, 0.17

--- rt-thread-am/bsp/abstract-machine/src/context.c
+++ rt-thread-am/bsp/abstract-machine/src/context.c
@@ -29,11 +29,10 @@ void __am_cte_init() {
     asm volatile ("csrw " # csr ", %0" : :  "r"(value))
 
 void rt_hw_context_switch_to(rt_ubase_t to) {
-  asm("yyyy:lw a0,0(a0)");
     //  uintptr_t* sp;
     //  sp=*(uintptr_t**)to;
     // // Context* to_c= sp;
-    Log("to=%d",to);
+    Log("to=%d",*(uintptr_t*)to);
 
     // CSR_WRITE(mepc,to_c->mepc);
 
