5297ffba4386323d53dadb08ca3c9eb4ba301e98
tracer-ysyx <tracer@ysyx.org>
2024-03-14 16:01:53 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:01:53 up 9 days, 56 min,  1 user,  load average: 0.44, 0.33, 0.19

--- rt-thread-am/bsp/abstract-machine/src/context.c
+++ rt-thread-am/bsp/abstract-machine/src/context.c
@@ -8,64 +8,80 @@
 
 #define STACK_ADDR (0x80000350)
 // #define STACK_ADDR 0x800229C8UL
-#define STACK_OFFSET(p) ((void*) (p) -(void*)STACK_ADDR)
+#define STACK_OFFSET(p) ((void *)(p) - (void *)STACK_ADDR)
 
-Context **__global_rt_to,**__global_rt_from;
+Context **__global_rt_to, **__global_rt_from;
 
-typedef struct _wrap_func_params {
-    void (*tentry)(void *parameter);
-    void *parameter;
-    void (*texit)(void);
+typedef struct _wrap_func_params
+{
+  void (*tentry)(void *parameter);
+  void *parameter;
+  void (*texit)(void);
 } wrap_func_params_t;
 
-static Context* ev_handler(Event e, Context *c) {
-  switch (e.event) {
-    case EVENT_YIELD:if(__global_rt_from!=(Context**)NULL) *__global_rt_from=c;c=*__global_rt_to;printf("ctx=%d\n",STACK_OFFSET(c)); break;
-    default: printf("Unhandled event ID = %d\n", e.event); assert(0);
+static Context *ev_handler(Event e, Context *c)
+{
+  switch (e.event)
+  {
+  case EVENT_YIELD:
+    if (__global_rt_from != (Context **)NULL)
+      *__global_rt_from = c;
+    c = *__global_rt_to;
+    printf("ctx=%d\n", STACK_OFFSET(c));
+    break;
+  default:
+    printf("Unhandled event ID = %d\n", e.event);
+    assert(0);
   }
   return c;
 }
 
-void __am_cte_init() {
+void __am_cte_init()
+{
   cte_init(ev_handler);
 }
 
 #define CSR_READ(csr, result) \
-    asm volatile ("csrr %0, " # csr : "=r"(result) : )
+  asm volatile("csrr %0, " #csr : "=r"(result) :)
 
 #define CSR_WRITE(csr, value) \
-    asm volatile ("csrw " # csr ", %0" : :  "r"(value))
+  asm volatile("csrw " #csr ", %0" : : "r"(value))
 
-void rt_hw_context_switch_to(rt_ubase_t to) {
-    //  uintptr_t* sp;
-    //  sp=*(uintptr_t**)to;
-    // // Context* to_c= sp;
-    Log("to=%d",*(uintptr_t*)to);
-    __global_rt_to=(Context**)to;
-    __global_rt_from=(Context**)NULL;
-    // CSR_WRITE(mepc,to_c->mepc);
+void rt_hw_context_switch_to(rt_ubase_t to)
+{
+  //  uintptr_t* sp;
+  //  sp=*(uintptr_t**)to;
+  // // Context* to_c= sp;
+  Log("to=%d", *(uintptr_t *)to);
+  __global_rt_to = (Context **)to;
+  __global_rt_from = (Context **)NULL;
+  // CSR_WRITE(mepc,to_c->mepc);
 
-    // asm("lw a0,0(a0)");
-    // asm("mv sp,a0");
-    // 触发一次自陷，以便在事件处理回调函数中进行上下文切换
-    yield();
+  // asm("lw a0,0(a0)");
+  // asm("mv sp,a0");
+  // 触发一次自陷，以便在事件处理回调函数中进行上下文切换
+  yield();
 }
 
-void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
-  static int i=0;
-  if(i++>=1) assert(0);
+void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to)
+{
+  static int i = 0;
+  if (i++ >= 1)
+    assert(0);
 
-  __global_rt_to=(Context**)to;
-  __global_rt_from=(Context**)from;
+  __global_rt_to = (Context **)to;
+  __global_rt_from = (Context **)from;
 
-    if(from<=0x80000000||to<=0x80000000||__global_rt_to<=(Context**)0x80000000||__global_rt_from<=(Context**)0x80000000){
-      assert(0);
-    }
+  if (from <= 0x80000000 || to <= 0x80000000 || __global_rt_to <= (Context **)0x80000000 || __global_rt_from <= (Context **)0x80000000)
+  {
+    assert(0);
+  }
 
   yield();
 }
 
-void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t to, struct rt_thread *to_thread) {
+void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t to, struct rt_thread *to_thread)
+{
   assert(0);
 }
 
@@ -73,47 +89,48 @@ void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t t
 #define CONTEXT_SIZE ((sizeof(uintptr_t) * 32) + sizeof(uintptr_t) * 4 /* + sizeof(wrap_func_params_t) */)
 
 
-
-static void wrap_entry(void *params)
+__attribute__((unused)) static void wrap_entry(void *params)
 {
-    wrap_func_params_t *p = (wrap_func_params_t *)params;
+  wrap_func_params_t *p = (wrap_func_params_t *)params;
 
-    p->tentry(p->parameter); // 调用入口函数
-    p->texit();              // 在tentry返回后调用退出函数
+  p->tentry(p->parameter); // 调用入口函数
+  p->texit();              // 在tentry返回后调用退出函数
 
-    while (1) {}
+  while (1)
+  {
+  }
 }
-rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter, rt_uint8_t *stack_addr, void *texit) {
-  //对齐stack
+rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter, rt_uint8_t *stack_addr, void *texit)
+{
+  // 对齐stack
   rt_uint8_t *unaligned_stack_addr = stack_addr; // 原始堆栈地址
   rt_uint8_t *aligned_stack_addr = (rt_uint8_t *)(((uintptr_t)unaligned_stack_addr + sizeof(uintptr_t) - 1) & ~(sizeof(uintptr_t) - 1));
 
-//wraper
-  aligned_stack_addr-=sizeof(wrap_func_params_t);
-    wrap_func_params_t *params_location = (wrap_func_params_t *)((uint8_t *)aligned_stack_addr);
-    // 初始化包装函数参数并保存到堆栈中
-    wrap_func_params_t params = { .tentry = tentry, .parameter = parameter, .texit = texit };
-    memcpy(params_location, &params, sizeof(params)); // 将参数复制到堆栈上的指定位置
+  // wraper
+  aligned_stack_addr -= sizeof(wrap_func_params_t);
+  wrap_func_params_t *params_location = (wrap_func_params_t *)((uint8_t *)aligned_stack_addr);
+  // 初始化包装函数参数并保存到堆栈中
+  wrap_func_params_t params = {.tentry = tentry, .parameter = parameter, .texit = texit};
+  memcpy(params_location, &params, sizeof(params)); // 将参数复制到堆栈上的指定位置
 
+  //Context *ctx = kcontext((Area){.start = aligned_stack_addr, .end = aligned_stack_addr}, wrap_entry, params_location);
+Context *ctx = kcontext((Area){.start = aligned_stack_addr, .end = aligned_stack_addr}, tentry, parameter);
 
-  Context *ctx = kcontext((Area){.start=aligned_stack_addr , .end=aligned_stack_addr},wrap_entry,params_location);
+  Log("aligned_stack_addr=%d ,stack_addr=%d ,ctx=%d,CONTEXT_SIZE=%d", STACK_OFFSET(aligned_stack_addr), STACK_OFFSET(stack_addr), STACK_OFFSET(ctx), CONTEXT_SIZE);
+  return (rt_uint8_t *)ctx;
 
-
-    Log("aligned_stack_addr=%d ,stack_addr=%d ,ctx=%d,CONTEXT_SIZE=%d",STACK_OFFSET(aligned_stack_addr),STACK_OFFSET(stack_addr),STACK_OFFSET(ctx),CONTEXT_SIZE);
-    return (rt_uint8_t *)ctx; 
-    
-    /**
-     * @brief stack:
-     *            
-     *            Context   <---sp & ctx
-     *             ...
-     *           sizeof(Context)
-     *             ...
-     *            wrap_func_params_t  <--- params_location
-     *             ...
-     *             xxxx  <---end
-     * 
-     */
+  /**
+   * @brief stack:
+   *
+   *            Context   <---sp & ctx
+   *             ...
+   *           sizeof(Context)
+   *             ...
+   *            wrap_func_params_t  <--- params_location
+   *             ...
+   *             xxxx  <---end
+   *
+   */
 }
 /* chatgpt 写的 */
 /* typedef struct _wrap_func_params {
