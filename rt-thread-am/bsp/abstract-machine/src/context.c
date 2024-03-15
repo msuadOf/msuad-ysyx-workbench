c6191b2538247502951ef441c70c560c5de416dc
tracer-ysyx <tracer@ysyx.org>
2024-03-15 09:08:33 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:08:33 up 9 days, 18:02,  1 user,  load average: 0.10, 0.13, 0.17

--- rt-thread-am/bsp/abstract-machine/src/context.c
+++ rt-thread-am/bsp/abstract-machine/src/context.c
@@ -49,7 +49,7 @@ static Context *ev_handler(Event e, Context *c)
       // (Context*)rt_interrupt_from_thread=c;
     }
     // c=*(Context**)rt_interrupt_to_thread;
-    c=(Context*)rt_interrupt_to_thread;
+    c=*(Context**)rt_interrupt_to_thread;
     // memcpy(c,*(Context**)rt_interrupt_to_thread,sizeof(Context));
   // }
 
