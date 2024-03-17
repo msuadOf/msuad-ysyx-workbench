055e9f15818fab191b224d50517bfee14ab38a62
tracer-ysyx <tracer@ysyx.org>
2024-03-17 18:25:26 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:25:26 up 12 days,  3:19,  1 user,  load average: 0.83, 1.04, 1.05

--- am-kernels/tests/cpu-tests/tests/mem_lwsw.c
+++ am-kernels/tests/cpu-tests/tests/mem_lwsw.c
@@ -1,23 +1,8 @@
 #include "trap.h"
 //char buf[512]={0};
 int main() {
-  printf("121\n");
-  volatile signed int x=-2147483648; volatile signed int y=-2147483648;
-  volatile signed int buf[20]={-2147483648,-2147483648,-2147483648,-2147483648,-2147483648,-2147483648,-2147483648,-2147483648,-2147483648,-2147483648,-2147483648,-2147483648};
-  for(int i=0;i<5;i++){
-    //buf[i]=x;
-    printf("(test)");
-    printf("hh %d\n",buf[i]);
-    //panic("");
-    //check(y==buf[i]);
-  }
-   printf("t: %d \n",-2147483648);
-   x=(volatile signed int)(x+y);
-   printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d(end)\n",1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6);
-   printf("%d %s %d %d %d\n",1,"23",3,4,5,6,7,8,9,0,1,2,3,4,5,6);
-  
-     printf("line %d: %s: %d  %s  %d  ==  %d =>  %s (%d)\n",1,"signed int",-2147483648,"+",-2147483648,0,"PASS",1212);
-printf("121\n");
-   printf("line %d: %s: %d  %s  %d  ==  %d =>  %s (%d)\n",__LINE__,"signed int",-2147483648,"+",-2147483648,0,(signed int)(x+y)==0?"PASS":"FAIL",(signed int)(x+y));
+asm("sw a0,0(a1)");
+asm("lw a0,0(a1)");
+
   return 0;
 }
