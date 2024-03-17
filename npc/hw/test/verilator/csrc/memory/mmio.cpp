03dd9bdeebb5dc8b8f5c23ffd917299d88b00d15
tracer-ysyx <tracer@ysyx.org>
2024-03-17 17:05:03 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:05:03 up 12 days,  1:59,  1 user,  load average: 0.39, 0.17, 0.17

--- npc/hw/test/verilator/csrc/memory/mmio.cpp
+++ npc/hw/test/verilator/csrc/memory/mmio.cpp
@@ -2,14 +2,14 @@
 #include "macro.h"
 
 #define IS_AT(DEVICE_NAME) concat(DEVICE_NAME,_mmio_is_at_serial)
-#define DEF_IF_ADDR(DEVICE_NAME) static inline bool IS_AT(DEVICE_NAME) (paddr_t addr, int len){\
+#define DEF_IF_ADDR(DEVICE_NAME) static inline int IS_AT(DEVICE_NAME) (paddr_t addr, int len){\
                           if(addr<=concat3(CONFIG_,DEVICE_NAME,_MMIO) || addr>=concat3(CONFIG_,DEVICE_NAME,_MMIO_END) ){\
-                            return false;\
+                            return 0;\
                           }\
                           if((addr+(len-1))<=concat3(CONFIG_,DEVICE_NAME,_MMIO) || (addr+(len-1))>=concat3(CONFIG_,DEVICE_NAME,_MMIO_END)){\
-                            return false;\
+                            return 0;\
                           }\
-                          return true;\
+                          return 1;\
                         }
 
 IFDEF(CONFIG_DEVICE_SERIAL,DEF_IF_ADDR(SERIAL)) 
