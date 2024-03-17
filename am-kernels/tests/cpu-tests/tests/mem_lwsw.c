ad4197506092274a34ed9862562e3ec50282fc7c
tracer-ysyx <tracer@ysyx.org>
2024-03-17 18:44:50 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:44:50 up 12 days,  3:39,  1 user,  load average: 0.91, 0.61, 0.68

--- am-kernels/tests/cpu-tests/tests/mem_lwsw.c
+++ am-kernels/tests/cpu-tests/tests/mem_lwsw.c
@@ -1,9 +1,13 @@
 #include "trap.h"
 //char buf[512]={0};
 int main() {
-
+asm("li a1,0x8000021c");
 asm("sw a0,0(a1)");
 asm("lw a0,0(a1)");
+asm("sw a0,0(a1)");
+asm("lw a0,0(a1)");
+
+asm("li a1,0x8000021c");
 
   return 0;
 }
