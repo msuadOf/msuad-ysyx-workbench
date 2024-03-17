8fe3d1f21ce3748a3c865aaa3dfb3c4137e1960e
tracer-ysyx <tracer@ysyx.org>
2024-03-17 18:12:10 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:12:10 up 12 days,  3:06,  1 user,  load average: 2.64, 1.79, 1.02

--- npc/hw/test/verilator/csrc/memory/mmio.cpp
+++ npc/hw/test/verilator/csrc/memory/mmio.cpp
@@ -20,7 +20,7 @@ IFDEF(CONFIG_DEVICE_RTC,DEF_IF_ADDR(RTC))
 #define doREAD(DEVICE_NAME) extern word_t mmio_read_SERIAL(paddr_t addr, int len);\
                             if(MMIO_IS_AT(DEVICE_NAME)) { return mmio_read_SERIAL(addr, len); }; 
 
-#define doWRITE(DEVICE_NAME)  extern void mmio_write_SERIAL(paddr_t addr, int len, word_t data);\
+#define doWRITE(DEVICE_NAME) extern void mmio_write_SERIAL(paddr_t addr, int len, word_t data);\
                                         if(MMIO_IS_AT(DEVICE_NAME)) {  mmio_write_SERIAL(addr, len,data); return; };
 
 word_t mmio_read(paddr_t addr, int len) {
