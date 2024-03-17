a913201aafbff724329e6ce0f4d296c956d0b799
tracer-ysyx <tracer@ysyx.org>
2024-03-17 17:04:42 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:04:41 up 12 days,  1:59,  1 user,  load average: 0.15, 0.12, 0.15

--- npc/hw/test/verilator/csrc/memory/mmio.cpp
+++ npc/hw/test/verilator/csrc/memory/mmio.cpp
@@ -3,10 +3,10 @@
 
 #define IS_AT(DEVICE_NAME) concat(DEVICE_NAME,_mmio_is_at_serial)
 #define DEF_IF_ADDR(DEVICE_NAME) static inline bool IS_AT(DEVICE_NAME) (paddr_t addr, int len){\
-                          if(addr>=concat3(CONFIG_,DEVICE_NAME,_MMIO) || addr<=concat3(CONFIG_,DEVICE_NAME,_MMIO_END) ){\
+                          if(addr<=concat3(CONFIG_,DEVICE_NAME,_MMIO) || addr>=concat3(CONFIG_,DEVICE_NAME,_MMIO_END) ){\
                             return false;\
                           }\
-                          if((addr+(len-1))>=concat3(CONFIG_,DEVICE_NAME,_MMIO) || (addr+(len-1))<=concat3(CONFIG_,DEVICE_NAME,_MMIO_END)){\
+                          if((addr+(len-1))<=concat3(CONFIG_,DEVICE_NAME,_MMIO) || (addr+(len-1))>=concat3(CONFIG_,DEVICE_NAME,_MMIO_END)){\
                             return false;\
                           }\
                           return true;\
@@ -25,8 +25,8 @@ word_t mmio_read(paddr_t addr, int len) {
 void mmio_write(paddr_t addr, int len, word_t data) {
 
     extern void mmio_write_serial(paddr_t addr, int len, word_t data);
-    //if(MMIO_IS_AT(SERIAL)) {  mmio_write_serial(addr, len,data); return; };
-    if(addr==CONFIG_SERIAL_MMIO) {  mmio_write_serial(addr, len,data); return; };
+    if(MMIO_IS_AT(SERIAL)) {  mmio_write_serial(addr, len,data); return; };
+    // if(addr==CONFIG_SERIAL_MMIO) {  mmio_write_serial(addr, len,data); return; };
 }
 
 /* 宏定义原型 */
