d5a2b8084d27117199a2438e125440d72aee1808
tracer-ysyx <tracer@ysyx.org>
2024-03-18 18:00:53 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:00:53 up 13 days,  2:55,  2 users,  load average: 2.39, 1.64, 1.61

--- npc/hw/test/verilator/csrc/sim_main.cpp
+++ npc/hw/test/verilator/csrc/sim_main.cpp
@@ -108,6 +108,10 @@ void isa_reg_display()
   }
   printf("%-8s%-#20x%-20d\n", "pc", s->pc, s->pc);
   printf("%-8s%-#20x%-20d\n", "dnpc", s->dnpc, s->dnpc);
+  printf("%-8s%-#20x%-20d\n", "dnpc", s->csr.mcause, s->csr.mcause);
+  printf("%-8s%-#20x%-20d\n", "dnpc", s->csr.mepc, s->csr.mepc);
+  printf("%-8s%-#20x%-20d\n", "dnpc", s->csr.mstatus, s->csr.mstatus);
+  printf("%-8s%-#20x%-20d\n", "dnpc", s->csr.mtvec, s->csr.mtvec);
   putchar('\n');
 }
 void isa_reg_display(CPU_state_diff_t* s)
