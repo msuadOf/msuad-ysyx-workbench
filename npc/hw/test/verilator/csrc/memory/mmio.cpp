b65ace609664b7660a1a3d86422aea0f7ef185be
tracer-ysyx <tracer@ysyx.org>
2024-03-18 08:54:14 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux ser332345018551 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:54:14 up 12 days, 17:48,  2 users,  load average: 1.89, 1.51, 1.51

--- npc/hw/test/verilator/csrc/memory/mmio.cpp
+++ npc/hw/test/verilator/csrc/memory/mmio.cpp
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "macro.h"
+#include "debug.h"
 
 #define IS_AT(DEVICE_NAME) concat(DEVICE_NAME,_mmio_is_at_serial)
 #define DEF_IF_ADDR(DEVICE_NAME) static inline int IS_AT(DEVICE_NAME) (paddr_t addr, int len){\
@@ -12,22 +13,30 @@
                           return 1;\
                         }
 
+/* add device addr checker here */
 IFDEF(CONFIG_DEVICE_SERIAL,DEF_IF_ADDR(SERIAL)) 
 IFDEF(CONFIG_DEVICE_RTC,DEF_IF_ADDR(RTC)) 
 
 /* bus interface */
 #define MMIO_IS_AT(DEVICE_NAME) IS_AT(DEVICE_NAME)(addr, len)
-#define doREAD(DEVICE_NAME) extern word_t mmio_read_SERIAL(paddr_t addr, int len);\
-                            if(MMIO_IS_AT(DEVICE_NAME)) { return mmio_read_SERIAL(addr, len); }; 
+  // eg. ifdef CONFIG_DEVICE_SERIAL
+#define doREAD(DEVICE_NAME) IFDEF(concat(CONFIG_DEVICE_,DEVICE_NAME),\
+                              extern word_t mmio_read_SERIAL(paddr_t addr, int len);\
+                              if(MMIO_IS_AT(DEVICE_NAME)) { return mmio_read_SERIAL(addr, len); }; \
+                              )
 
-#define doWRITE(DEVICE_NAME) extern void mmio_write_SERIAL(paddr_t addr, int len, word_t data);\
-                                        if(MMIO_IS_AT(DEVICE_NAME)) {  mmio_write_SERIAL(addr, len,data); return; };
+#define doWRITE(DEVICE_NAME)  IFDEF(concat(CONFIG_DEVICE_,DEVICE_NAME),\
+                                        extern void mmio_write_SERIAL(paddr_t addr, int len, word_t data);\
+                                        if(MMIO_IS_AT(DEVICE_NAME)) {  mmio_write_SERIAL(addr, len,data); return; }; \
+                                  )
 
 word_t mmio_read(paddr_t addr, int len) {
     // extern word_t mmio_read_SERIAL(paddr_t addr, int len);
     // if(MMIO_IS_AT(SERIAL)) { return mmio_read_SERIAL(addr, len); };
     doREAD(SERIAL);
     doREAD(RTC);
+    panic("[npc]mmio_read should not reach here!");
+    return -1;
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
@@ -35,6 +44,7 @@ void mmio_write(paddr_t addr, int len, word_t data) {
     // if(MMIO_IS_AT(SERIAL)) {  mmio_write_SERIAL(addr, len,data); return; };
     doWRITE(SERIAL);
     doWRITE(RTC);
+    panic("[npc]mmio_write should not reach here!");
 }
 
 /* 宏定义原型 */
