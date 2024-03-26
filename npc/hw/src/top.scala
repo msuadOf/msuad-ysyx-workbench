dc1127f37c83110a34454e4ebca182fab0f9f591
tracer-ysyx <tracer@ysyx.org>
2024-03-26 21:44:07 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux Lsuad 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:44:07 up  8:11,  1 user,  load average: 0.46, 0.34, 0.22

--- npc/hw/src/top.scala
+++ npc/hw/src/top.scala
@@ -4,21 +4,56 @@ import chisel3.util._
 import chisel3.util.experimental._
 import chisel3.experimental.prefix
 
-class test_12 extends Module{
-  val io=IO(Flipped (new mmioIO))
- 
-io.AR.arWidth:=0.U
-io.AR.arValid:=0.U
-io.AR.arAddr:=0.U
+class test_12 extends Module {
+  val io = IO(Flipped(new mmioIO))
 
-io.R.rReady:=0.U
+  io.AR.arWidth := 0.U
+  io.AR.arValid := 0.U
+  io.AR.arAddr  := 0.U
 
-io.simpleW.wAddr :=0.U
-io.simpleW.wData :=0.U
-io.simpleW.wWidth:=0.U
-io.simpleW.wValid:=0.U
+  io.R.rReady := 0.U
 
+  io.simpleW.wAddr  := 0.U
+  io.simpleW.wData  := 0.U
+  io.simpleW.wWidth := 0.U
+  io.simpleW.wValid := 0.U
 
+  val I_en = RegInit(1.U)
+
+  val sIDLE :: sARwaiting :: sARcplt_Rwaiting :: sRcplt :: Nil = Enum(4)
+  val R_state                                                  = RegInit(sIDLE)
+//跳转
+// R_state:=sIDLE
+  switch(R_state) {
+    is(sIDLE) {
+      when(I_en === 1.U) {
+        R_state := sARwaiting
+      }.otherwise {
+        R_state := sIDLE
+      }
+    }
+    is(sARwaiting) {
+      when(io.AR.arReady===1.U){
+        R_state:=sARcplt_Rwaiting
+      }.otherwise{
+        R_state:=sARwaiting
+      }
+    }
+    is(sARcplt_Rwaiting) {
+      when(io.R.rValid===1.U){
+        R_state:=sRcplt
+      }.otherwise{
+        R_state:=sARcplt_Rwaiting
+      }
+    }
+    is(sRcplt) {
+      when(I_en===1.U){
+        R_state:=sARwaiting
+      }.otherwise{
+        R_state:=sRcplt
+      }
+    }
+  }
 }
 class top(isa_info: String = "RISCV32") extends Module {
   val io = IO(new Bundle {
@@ -31,19 +66,18 @@ class top(isa_info: String = "RISCV32") extends Module {
       val snpc = Output(UInt(32.W))
       val regs = Output(Vec(32, UInt(32.W)))
 
-      val mepc =    Output(UInt(32.W))
-      val mcause =  Output(UInt(32.W))  
-      val mstatus = Output(UInt(32.W))   
-      val mtvec =   Output(UInt(32.W)) 
+      val mepc    = Output(UInt(32.W))
+      val mcause  = Output(UInt(32.W))
+      val mstatus = Output(UInt(32.W))
+      val mtvec   = Output(UInt(32.W))
     }
   })
 
-  val core=Module(new Core)
-  core.io<>this.io
+  val core = Module(new Core)
+  core.io <> this.io
 
-  
-  val mmio_dpi=Module(new mmio_dpi_wraper)
-  val test_12=Module(new test_12)
+  val mmio_dpi = Module(new mmio_dpi_wraper)
+  val test_12  = Module(new test_12)
 
-  mmio_dpi.io<>test_12.io
+  mmio_dpi.io <> test_12.io
 }
