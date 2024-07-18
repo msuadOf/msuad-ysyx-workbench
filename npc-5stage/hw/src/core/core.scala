60323c9f4c09801867266bc8a0eb3f85b01f5afd
tracer-ysyx <tracer@ysyx.org>
2024-07-18 15:39:19 +0000

    > compile NEMU ysyx_23060093 白逸凡 Linux localhostubuntu 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:39:19 up 1 day, 39 min,  0 users,  load average: 2.64, 1.08, 0.66

--- npc-5stage/hw/src/core/core.scala
+++ npc-5stage/hw/src/core/core.scala
@@ -79,6 +79,7 @@ class Core extends Module {
   IDStage.build()
   EXStage.build()
   WBStage.build()
+  io.diff.pc:=IFStage.pc.read
   val isInsertReg = false
   StageConnect(withRegBeats = isInsertReg)(IFStage, IDStage)
   StageConnect(withRegBeats = isInsertReg)(IDStage, EXStage)
