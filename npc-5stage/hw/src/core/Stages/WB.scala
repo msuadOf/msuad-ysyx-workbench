763f9587f800bada903e861734a498ba2db92e25
tracer-ysyx <tracer@ysyx.org>
2024-07-18 13:31:09 +0000

    > sim npc ysyx_23060093 白逸凡 Linux localhostubuntu 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:31:09 up 22:31,  0 users,  load average: 1.61, 1.13, 0.74

--- npc-5stage/hw/src/core/Stages/WB.scala
+++ npc-5stage/hw/src/core/Stages/WB.scala
@@ -5,20 +5,24 @@ import core._
 import core.utils.BundlePlus
 import chisel3._
 
-class WriteBackStage(_in: EX2WBBundle, regfile: RegFile, pc: PC,diff:diffIO) extends Stage(_in, new BundlePlus {}) {
-  def this(_in: EX2WBBundle, regfile: RegFile, pc: PC)={
-    this(_in, regfile, pc,null)
+class WriteBackStage(_in: EX2WBBundle, regfile: RegFile, pc: PC, diff: diffIO) extends Stage(_in, new BundlePlus {}) {
+  def this(_in: EX2WBBundle, regfile: RegFile, pc: PC) = {
+    this(_in, regfile, pc, null)
   }
   override def build(): Unit = {
-    in.ready:=1.B
-    val ebreak=Module(new Ebreak)
+    in.ready := 1.B
+    val ebreak = Module(new Ebreak)
     ebreak(in.fire && in.bits.ebreak)
     regfile.write(in.fire && in.bits.RrdEn, in.bits.rd, in.bits.Rrd)
     pc.write(in.fire && in.bits.dnpcEn, in.bits.dnpc)
 
-    if(diff!=null){
-      diff.regs:=regfile.reg
-      diff.pc:=pc.pc
+    if (diff != null) {
+      when(in.fire) {
+        diff.regs    := regfile.reg
+        diff.pc      := pc.pc
+        diff.dnpc    := pc.dnpc
+        diff.diff_en := 1.U
+      }
     }
   }
 }
