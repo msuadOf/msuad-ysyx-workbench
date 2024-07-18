aeeed6ae2e00dd9b98156c33bcf277f35be331d9
tracer-ysyx <tracer@ysyx.org>
2024-07-18 14:25:03 +0000

    > sim npc ysyx_23060093 白逸凡 Linux localhostubuntu 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:25:03 up 23:24,  0 users,  load average: 0.26, 0.44, 0.62

--- npc-5stage/hw/test/verilator/csrc/difftest/difftest.mk
+++ npc-5stage/hw/test/verilator/csrc/difftest/difftest.mk
@@ -1,7 +1,7 @@
 # CONFIG_DIFFTEST=1
 
 ifdef CONFIG_DIFFTEST
-DIFF_REF_SO = $(NEMU_HOME)/build/riscv32-nemu-interpreter-so
+DIFF_REF_SO = ${NEMU_HOME}/build/riscv32-nemu-interpreter-so
 MKFLAGS ?= #GUEST_ISA=$(GUEST_ISA) SHARE=1 ENGINE=interpreter
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
 
@@ -9,7 +9,7 @@ ARGS_DIFF = --diff=$(DIFF_REF_SO)
 $(DIFF_REF_SO):
 	@echo + BUILD difftest-reference
 	
-	$(MAKE) -s -C $(NEMU_HOME) $(MKFLAGS)
+	${MAKE} -s -C $(NEMU_HOME) $(MKFLAGS)
 
 .PHONY: $(DIFF_REF_SO)
 endif
