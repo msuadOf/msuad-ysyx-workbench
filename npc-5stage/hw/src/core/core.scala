5b2da47ba2dab02cf28565a9426e2301fd182847
tracer-ysyx <tracer@ysyx.org>
2024-07-19 19:25:19 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux VM-16-13-ubuntu 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:25:19 up  1:14,  2 users,  load average: 1.59, 0.46, 0.24

--- npc-5stage/hw/src/core/core.scala
+++ npc-5stage/hw/src/core/core.scala
@@ -92,7 +92,7 @@ class Core extends Module {
   scoreBoard.id_record(id_out.rd,id_out.rd_en)
   scoreBoard.wb_record(WBStage.in.bits.rd,WBStage.in.bits.RrdEn)
   when(scoreBoard.id_judgeRAW(id_out.rs1,id_out.rs1_en,id_out.rs2,id_out.rs2_en)()){
-      EXStage.in.ready := 0.B
+      IDStage.in.ready := 0.B
   }
   io.monitor.scoreBoard:= Cat(scoreBoard.regfileBusy)
   io.monitor.connectStageIO(IFStage)
--- npc-5stage/hw/test/verilator/csrc/runtime/dpi.cpp
+++ npc-5stage/hw/test/verilator/csrc/runtime/dpi.cpp
@@ -2,11 +2,9 @@
 
 extern void hit_exit(int status);
 
-int is_ebreak=0;
 extern "C" void ebreak(){
     puts(ANSI_FG_GREEN);
     puts("npc:excute the ebreak inst\n");
     puts(ANSI_NONE);
-    //is_ebreak=1;
     hit_exit(0);
 }
