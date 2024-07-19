13f5ad7c962abdd1ee4d36e237da672e53110165
tracer-ysyx <tracer@ysyx.org>
2024-07-19 19:02:41 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux VM-16-13-ubuntu 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:02:41 up 52 min,  2 users,  load average: 0.30, 0.20, 0.19

--- npc-5stage/hw/test/verilator/csrc/runtime/dpi.cpp
+++ npc-5stage/hw/test/verilator/csrc/runtime/dpi.cpp
@@ -1,12 +1,12 @@
 #include "r_verilator.h"
 
-extern void hit_exit(int status);
+// extern void hit_exit(int status);
 
 int is_ebreak=0;
 extern "C" void ebreak(){
     puts(ANSI_FG_GREEN);
     puts("npc:excute the ebreak inst\n");
     puts(ANSI_NONE);
-    //is_ebreak=1;
-    hit_exit(0);
+    is_ebreak=1;
+    // hit_exit(0);
 }
