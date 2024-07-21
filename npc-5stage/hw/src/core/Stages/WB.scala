a7616131d2c0695ed6094fe0c26ebf93e6b0c05d
tracer-ysyx <tracer@ysyx.org>
2024-07-21 16:23:03 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux VM-16-13-ubuntu 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:23:03 up 1 day, 20:45,  1 user,  load average: 0.94, 0.89, 0.80

--- npc-5stage/hw/src/core/Stages/WB.scala
+++ npc-5stage/hw/src/core/Stages/WB.scala
@@ -14,8 +14,7 @@ class WriteBackStage(_in: EX2WBBundle, regfile: RegFile, pc: PC, diff: diffIO) e
     val ebreak = Module(new Ebreak)
     ebreak(in.fire && in.bits.ebreak)
     regfile.write(in.fire && in.bits.RrdEn, in.bits.rd, in.bits.Rrd)
-    //pc.write(in.fire && in.bits.dnpcEn, in.bits.dnpc)
-    pc.write(in.fire , Mux(in.bits.dnpcEn,in.bits.dnpc,in.bits.pc+4.U) )
+    pc.write(in.fire && in.bits.dnpcEn, in.bits.dnpc)
 
     //if (diff != null) {
         // diff.regs    := regfile.reg
