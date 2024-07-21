f1694e0e97de3e664b66ffd4a93366b1cc353e28
tracer-ysyx <tracer@ysyx.org>
2024-07-21 16:32:49 +0800

    > compile NEMU ysyx_23060093 白逸凡 Linux VM-16-13-ubuntu 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:32:49 up 1 day, 20:55,  1 user,  load average: 2.46, 1.47, 1.02

--- npc-5stage/hw/src/core/core.scala
+++ npc-5stage/hw/src/core/core.scala
@@ -94,7 +94,7 @@ class Core extends Module {
   scoreBoard.id_record(id_out.rd,id_out.rd_en,IDStage.out.fire)
   scoreBoard.wb_record(WBStage.in.bits.rd,WBStage.in.bits.RrdEn,WBStage.in.fire)
   val sb_out : ()=>Bool =scoreBoard.id_judgeRAW(id_out.rs1,id_out.rs1_en,id_out.rs2,id_out.rs2_en)
-  when(scoreBoard.id_judgeRAW(id_out.rs1,id_out.rs1_en,id_out.rs2,id_out.rs2_en)()){
+  when(IDStage.in.valid &&scoreBoard.id_judgeRAW(id_out.rs1,id_out.rs1_en,id_out.rs2,id_out.rs2_en)()){
      IDStage.out.valid:=0.B
     IDStage.out.ready:=0.B
   }
